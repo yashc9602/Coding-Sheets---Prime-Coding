32ba9e6d333404f8992d4350a38f4ac597ff0ff1
Aditya-Mishra19 <skadityamishra@gmail.com>
2024-07-24 13:31:49 +0530

    changed Java Code

--- Intersection of Two Arrays/java.java
+++ Intersection of Two Arrays/java.java
@@ -1,31 +1,34 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Main {
+public class Intersection {
+
     public static List<Integer> intersection(int[] arr1, int[] arr2) {
-        List<Integer> result = new ArrayList<>();
-        for (int num1 : arr1) {
-            boolean found = false;
-            for (int num2 : arr2) {
-                if (num1 == num2) {
-                    found = true;
-                    break;
-                }
-            }
-            if (found && !result.contains(num1)) {
-                result.add(num1);
+        int i = 0, j = 0;
+        List<Integer> ans = new ArrayList<>();
+        
+        while (i < arr1.length && j < arr2.length) {
+            if (arr1[i] < arr2[j]) {
+                i++;
+            } else if (arr2[j] < arr1[i]) {
+                j++;
+            } else {
+                ans.add(arr1[i]);
+                i++;
+                j++;
             }
         }
-        return result;
+        
+        return ans;
     }
 
     public static void main(String[] args) {
         int[] arr1 = {1, 2, 2, 3, 4};
         int[] arr2 = {2, 2, 3, 5};
         List<Integer> result = intersection(arr1, arr2);
+        
         for (int num : result) {
             System.out.print(num + " ");
         }
-        System.out.println();  // Output: 2 3
     }
 }
