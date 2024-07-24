04d8fb5876578ab740e527ef66d4dba5a3186e9d
Aditya-Mishra19 <skadityamishra@gmail.com>
2024-07-24 13:34:02 +0530

    changed code CPP and JAVA

--- Intersection of Two Arrays/python.py
+++ Intersection of Two Arrays/python.py
@@ -1,9 +1,16 @@
 def intersection(arr1, arr2):
-    result = []
-    for num in arr1:
-        if num in arr2 and num not in result:
-            result.append(num)
-    return result
+    i, j = 0, 0 
+    ans = []
+    while i < len(arr1) and j < len(arr2):
+        if arr1[i] < arr2[j]: # 1 < 2 
+            i += 1
+        elif arr2[j] < arr1[i]:
+            j += 1
+        else:
+            ans.append(arr1[i])
+            i += 1
+            j += 1
+    return ans
 
-# Test
-print(intersection([1, 2, 2, 3, 4], [2, 2, 3, 5]))  # Output: [2, 3]
+
+print(*intersection([1, 2, 2, 3, 4], [2, 2, 3, 5]))
