640b4b95f7c78f21e527bc8849faa4ce23875988
Aditya-Mishra19 <skadityamishra@gmail.com>
2024-07-25 16:20:20 +0530

    modified the file

--- Part 1/finds the largest number that can be formed by permutation/cpp.cpp
+++ Part 1/finds the largest number that can be formed by permutation/cpp.cpp
@@ -1,37 +1,20 @@
-// Write a program that accepts the integer array of length ‘size’ and finds the largest number that can be formed by permutation.
-
 #include <iostream>
+#include <algorithm>
 #include <vector>
-#include <string>
-using namespace std;
-
-bool compare(int a, int b) {
-    string ab = to_string(a) + to_string(b);
-    string ba = to_string(b) + to_string(a);
-    return ab > ba;
-}
-
-void custom_sort(vector<int> &arr) {
-    for (int i = 0; i < arr.size(); i++) {
-        for (int j = i + 1; j < arr.size(); j++) {
-            if (!compare(arr[i], arr[j])) {
-                swap(arr[i], arr[j]);
-            }
-        }
-    }
-}
-
-string largest_number(vector<int> &arr) {
-    custom_sort(arr);
-    string result;
-    for (int num : arr) {
-        result += to_string(num);
-    }
-    return result[0] == '0' ? "0" : result;
-}
 
 int main() {
-    vector<int> arr = {3, 30, 34, 5, 9};
-    cout << "The largest number formed is " << largest_number(arr) << endl;  // Output should be 9534330
+    std::string str;
+    std::getline(std::cin, str);
+    std::string newstr;
+    for (char c : str) {
+        if (c != ' ') {
+            newstr += c;
+        }
+    }
+    std::vector<char> arr(newstr.begin(), newstr.end());
+    std::sort(arr.begin(), arr.end());
+    for (auto it = arr.rbegin(); it != arr.rend(); ++it) {
+        std::cout << *it;
+    }
     return 0;
 }
