bf2ce0baae4a1a5fa86158048126cb03cc93e035
Nishant khanderao <123276454+Khanderaonishant@users.noreply.github.com>
2024-08-17 01:27:04 +0530

    Update cpp.cpp

--- Accenture/Array Part 1, 2/sum of the divisors for the N integer number/cpp.cpp
+++ Accenture/Array Part 1, 2/sum of the divisors for the N integer number/cpp.cpp
@@ -1,18 +1,13 @@
-#include <iostream>
-using namespace std;
-
-int sum_of_divisors(int N) {
-    int sum_div = 0;
-    for (int i = 1; i <= N; i++) {
-        if (N % i == 0) {
-            sum_div += i;
+class Solution
+{
+public:
+    long long sumOfDivisors(int N)
+    {
+        long long sum=0;
+        for(int i=1;i<=N;i++)
+        {
+          sum+=(N/i)*i;  
         }
+        return sum;
     }
-    return sum_div;
-}
-
-int main() {
-    int N = 12;
-    cout << "The sum of the divisors of " << N << " is " << sum_of_divisors(N) << endl;  // Output should be 28
-    return 0;
-}
+};
